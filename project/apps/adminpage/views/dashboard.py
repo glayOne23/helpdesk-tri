3491c6765f8f163c7daec96bbc9631fefeca8081
Muhammad Hammam Islami, S.Kom <mhi595@ums.ac.id>
2024-08-09 10:51:31 +0700

    [feat] tambah chart

--- project/apps/adminpage/views/dashboard.py
+++ project/apps/adminpage/views/dashboard.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
-from apps.services.decorators import group_required
-from django.contrib import messages
-from django.urls import reverse
-from json import dumps
+import json
 
+from django.shortcuts import render
+from django.db.models import Count, Prefetch
+
+
+
+from apps.adminpage.models import Ticket, TicketAdmin, TicketCategory, TicketState
 
 
 
@@ -30,18 +32,28 @@ def index(request):
     context['duallistbox']      = False
     context['daterange']        = False
     context['countdown']        = False
+    context['chart']            = True
 
 
     # Example Notification
     # extra_tags is optional, used in the notif function located in -> (apps\adminpage\templates\adminpage\layout\javascript.html)
 
-    # messages.success(request, 'Welcome to Dashboard', extra_tags=dumps({
-    #     'redirect'      : reverse('adminpage:dashboard'), 
-    #     'confbtntxt'    : 'Check Data',
-    #     'cnclbtntxt'    : 'Cancel',
-    #     'title'         : 'Welcome',
-    # }))
+    if request.user.groups.filter(name__in=['admin']).exists() or request.user.is_superuser:
+        dataticket = Ticket.objects.all()
+    elif request.user.groups.filter(name__in=['bpsdm']).exists():
+        ticketcategories = TicketAdmin.objects.filter(user=request.user).values('ticketcategory')
+        dataticket = Ticket.objects.filter(category__in=ticketcategories)
+    else:
+        dataticket = Ticket.objects.filter(user=request.user)
 
+    context['totalticket'] = dataticket.count()
+    context['stateticket'] = (
+        TicketState.objects
+        .prefetch_related(Prefetch('ticket_set', queryset=dataticket))
+        .annotate(ticket_count=Count('ticket'))
+        .values('name', 'ticket_count')
+    )
+    context['stateticket'] = json.dumps(list(context['stateticket']))
 
     # ===[Render Template]===
     return render(request, 'adminpage/index.html', context)
